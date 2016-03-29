41817ef31ff34b34ec65e045e0bfad856ee9fa84
Hina Aman <hina.aman@gmail.com>
2016-03-28 20:17:53 -0700

    Add UDP listner

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ var io = require('socket.io',{ rememberTransport: false, transports: ['WebSocket
 var port = process.env.PORT || 3000;
 server.listen(port);
 
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
+// var ObjectId = require('mongodb').ObjectID;
+// var url = 'mongodb://localhost:27017';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
+
+var PORT = 5005;
+var HOST = 'localhost';
+var dgram = require('dgram');
+var dserver = dgram.createSocket('udp4');
+dserver.bind(PORT);
+dserver.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
 });
 
 app.get('/', function (req, res) {
